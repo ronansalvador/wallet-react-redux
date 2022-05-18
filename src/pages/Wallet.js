4dc0c1e882ff1f747f22586cb9b76f461233b0c9
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-05-18 18:06:32 -0300

    Req 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -12,12 +12,61 @@ class Wallet extends React.Component {
 
   render() {
     const { currencies } = this.props;
-    const teste = currencies.filter((moeda) => moeda !== 'USDT');
     console.log(currencies);
-    console.log(teste);
     return (
       <div>
         <Header />
+        <form>
+          <label htmlFor="despesa">
+            Valor
+            <input
+              data-testid="value-input"
+              type="number"
+              id="despesa"
+              placeholder="despesa"
+            />
+          </label>
+          <label htmlFor="currencies">
+            Moeda
+            <select id="currencies">
+              {currencies.map((currencie) => (
+                <option
+                  key={ currencie }
+                  value={ currencie }
+                >
+                  {currencie}
+
+                </option>))}
+            </select>
+          </label>
+          <label htmlFor="metodo-pagamento">
+            Método de pagamento:
+            <select data-testid="method-input" id="metodo-pagamento">
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="categoria">
+            Categoria
+            <select data-testid="tag-input" id="categoria">
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saude">Saúde</option>
+            </select>
+          </label>
+          <label htmlFor="descricao">
+            Descrição
+            <input
+              data-testid="description-input"
+              type="text"
+              id="descricao"
+              placeholder="despesa"
+            />
+          </label>
+        </form>
       </div>);
   }
 }
