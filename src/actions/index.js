dacbf4135d25d70e14b16d8e071222f38c16615f
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-05-18 17:39:20 -0300

    Req 4

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 // Coloque aqui suas actions
 export const ADD_USER = 'ADD_USER';
-// export const ADD_PROFESSIONAL = 'ADD_PROFESSIONAL';
+export const GET_CURRENCIES = 'GET_CURRENCIES';
 
 export const addUserAction = (email) => ({
   type: ADD_USER,
@@ -13,3 +13,23 @@ export const addUserAction = (email) => ({
 //     payload: state,
 //   };
 // }
+
+export const getCurrencies = (currencies) => ({
+  type: GET_CURRENCIES,
+  currencies,
+});
+
+export const getcurrenciesThunk = () => {
+  console.log('Redux Thunk');
+  return async (dispatch) => {
+    try {
+      const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+      const data = await response.json();
+      const allCurrencies = Object.keys(data);
+      const currencies = allCurrencies.filter((moeda) => moeda !== 'USDT');
+      dispatch(getCurrencies(currencies));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
+import { getcurrenciesThunk } from '../actions';
 
 class Wallet extends React.Component {
+  componentDidMount() {
+    const { getCurrencies } = this.props;
+    getCurrencies();
+  }
+
   render() {
+    const { currencies } = this.props;
+    const teste = currencies.filter((moeda) => moeda !== 'USDT');
+    console.log(currencies);
+    console.log(teste);
     return (
       <div>
         <Header />
@@ -10,4 +22,17 @@ class Wallet extends React.Component {
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  currencies: state.wallet.currencies,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getCurrencies: () => dispatch(getcurrenciesThunk()),
+});
+
+Wallet.propTypes = {
+  currencies: PropTypes.arrayOf.isRequired,
+  getCurrencies: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
