15e18850ffa9e46e3b3909b45d5298d3968b6a00
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-05-18 16:41:48 -0300

    Requisito 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
 // Coloque aqui suas actions
+export const ADD_USER = 'ADD_USER';
+// export const ADD_PROFESSIONAL = 'ADD_PROFESSIONAL';
+
+export const addUserAction = (email) => ({
+  type: ADD_USER,
+  email,
+});
+
+// export function addProfessionalAction(state) {
+//   return {
+//     type: ADD_PROFESSIONAL,
+//     payload: state,
+//   };
+// }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addUserAction } from '../actions';
 
-class Login extends React.Component {
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      checkbutton: true,
+    };
+  }
+
+  handleLogin = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    }, () => {
+      const { email, password } = this.state;
+      const validateEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      const validatePassword = 6;
+
+      this.setState({
+        checkbutton: !(validateEmail.test(email) && password.length >= validatePassword),
+      });
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { history, addUser } = this.props;
+    const { email } = this.state;
+    addUser(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { checkbutton, email, password } = this.state;
+
+    return (
+      <div>
+        <form>
+          <input
+            type="email"
+            data-testid="email-input"
+            name="email"
+            value={ email }
+            placeholder="E-mail"
+            onChange={ this.handleLogin }
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            name="password"
+            value={ password }
+            onChange={ this.handleLogin }
+          />
+          <button
+            type="submit"
+            disabled={ checkbutton }
+            onClick={ this.handleSubmit }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>);
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  addUser: PropTypes.func.isRequired,
+};
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addUser: (email) => dispatch(addUserAction(email)),
+  };
+}
+
+export default connect(null, mapDispatchToProps)(Login);
